3e88e5ed1b0629f36ce3dcdabeea5ae3232523df
Nate Ensign <dixienate@gmail.com>
2024-05-07 11:24:01 -0600

    responsive navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,43 @@
-import React from 'react'
-import './navbar.scss'
+import React, { useState } from "react";
+import "./navbar.scss";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
-        <div className="left">Left</div>
-        <div className="right">Right</div>
+      <div className="left" >
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="68_N8-Estate Logo" />
+          <span>68_N8-Estate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a href="/" className="register">
+          Sign Up
+        </a>
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt="Menu"
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign in</a>
+          <a href="/">Sign up</a>
+        </div>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
